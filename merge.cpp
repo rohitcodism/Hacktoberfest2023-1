e65fce2ff674713f4909d9ec24c86bfc269d598e
Rohit Paul <111235103+rohitcodism@users.noreply.github.com>
2023-10-06 07:17:21 +0530

    Create merge.cpp

---
+++ merge.cpp
@@ -0,0 +1,85 @@
+// merge sort
+
+#include <iostream>
+#include <vector>
+
+// Function to merge two subarrays of arr[]
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(std::vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temporary arrays
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temporary arrays back into arr[l..r]
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Recursive function to perform mergesort
+void mergeSort(std::vector<int>& arr, int l, int r) {
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for large l and r
+        int m = l + (r - l) / 2;
+
+        // Recursively sort the first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the sorted halves
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    int n = arr.size();
+
+    std::cout << "Original array:";
+    for (int num : arr) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
+
+    mergeSort(arr, 0, n - 1);
+
+    std::cout << "Sorted array:";
+    for (int num : arr) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
